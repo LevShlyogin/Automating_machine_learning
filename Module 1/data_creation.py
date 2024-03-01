7d031bf0832232bf5da353eb4197b7c3f5d42b8c
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-03-01 23:29:49 +0500

    Update data_creation.py

--- Module 1/data_creation.py
+++ Module 1/data_creation.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
 
-
-data = pd.read_csv("cleaned_car.csv", delimiter = ',', index_col=0)
+data = pd.read_csv('dataset/quicker-car-cleaned-dataset/cleaned_car.csv', delimiter = ',', index_col=0)
 
 # Смотрим на типы данных
 cat_columns = []
@@ -26,3 +27,20 @@ df_se[cat_columns] = df_se[cat_columns].astype('category')
 for _, column_name in enumerate(cat_columns):
     df_se[column_name] = df_se[column_name].cat.codes
 
+# Определяем X и y, нам нужно предсказать цену
+y = df_se["Price"]
+X = df_se.drop(["Price"], axis = 1)
+
+# Делим данные на тестовую и тренировочную выборки
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Сохраняем тренировочные и тестовые выборки в CSV файлы
+train_data = pd.concat([X_train, y_train], axis=1)
+test_data = pd.concat([X_test, y_test], axis=1)
+
+# Создаём папку для сохранения данных
+os.makedirs("dataset/train", exist_ok=True)
+os.makedirs("dataset/test", exist_ok=True)
+
+train_data.to_csv('dataset/train/train_data.csv', index=False)
+test_data.to_csv('dataset/test/test_data.csv', index=False)
