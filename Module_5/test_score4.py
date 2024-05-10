4ac4ed322288dcb8b12a08ad910600b5bf75c416
georgy-sidorkin <gosha.sidorkin13@gmail.com>
2024-05-10 17:39:54 +0500

    complete lab 5

---
+++ Module_5/test_score4.py
@@ -0,0 +1,19 @@
+import pytest
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
+
+@pytest.fixture()
+def load_ys():
+    ys = np.loadtxt("ys4.csv", delimiter=";")
+    return ys
+
+@pytest.fixture()
+def load_pred():
+    pred = np.loadtxt("pred4.csv", delimiter=";")
+    return pred
+
+def test_mse(load_ys, load_pred):
+    assert mean_squared_error(load_ys, load_pred) < 1
+    
+def test_r2(load_ys, load_pred):
+    assert r2_score(load_ys, load_pred) > 0.9
